95369285521a976eec2568602c6973db3ae1d467
HDreamer2 <651773341@qq.com>
2024-06-18 22:07:51 +0800

    逻辑回归

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import os
 from Linear_Regression import LinearRegression
+from LogisticRegression import LogisticRegression
 import pandas as pd
 import json
 
@@ -52,13 +53,37 @@ def linear_regression_train():
 
     return jsonify({'message': 'Training started successfully'}), 200
 
-#暂时不用这个接口
-@app.route('/linear-regression/get-epoch-data', methods=['GET'])
-def get_epoch_data():
-    # 假设epoch_data是全局变量或存储在某处
-    # global epoch_data
-    # return jsonify(epoch_data), 200
-    return
+
+# @app.route('/linear-regression/get-epoch-data', methods=['GET'])
+# def get_epoch_data():
+#     # 假设epoch_data是全局变量或存储在某处
+#     # global epoch_data
+#     # return jsonify(epoch_data), 200
+#     return
+
+@app.route('/logistic-regression/train', methods=['POST'])
+def logic_regression_train():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
+
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+
+    file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+    file.save(file_path)
+
+    data = request.form
+
+    features = json.loads(data.get('features'))
+    label = [data.get('label')]
+    epochs = int(data.get('epoch'))
+
+    data = pd.read_csv(file_path)
+    LogisticRegression(data, features, label, epochs)
+
+    return jsonify({'message': 'Training started successfully'}), 200
+
 
 
 if __name__ == '__main__':
