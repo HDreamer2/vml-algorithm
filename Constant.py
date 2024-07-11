1608a296d144fe156871c22dc5136052fb451c52
ysxu666 <70496853+ysxu666@users.noreply.github.com>
2024-07-11 15:03:24 +0800

    Update Constant.py

--- Constant.py
+++ Constant.py
@@ -5,3 +5,4 @@ LINEAR_REGRESSION_GET_EPOCH_DATA = JAVA_URL + "/linear-regression/get-epoch-data
 LOGISTIC_REGRESSION_GET_EPOCH_DATA = JAVA_URL + "/logistic-regression/get-epoch-data"
 
 DECISION_TREE_GET_DATA = JAVA_URL + "/decision-tree/get-data"
+RANDOM_FOREST_GET_DATA = JAVA_URL + "/random-forest/get-data"
